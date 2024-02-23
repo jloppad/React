c0ba4e497d99b3693efd10f335267b8a590803e5
jloppad <jloppad@g.educannd.es>
2024-02-23 10:06:34 +0100

    Correccion

--- galeria/src/App.jsx
+++ galeria/src/App.jsx
@@ -9,47 +9,27 @@ import img6 from "./recursos/6.jpg";
 
 function App() {
 
+  const imgs = [
+    {agua: true, src: img1, alt:"foto1", desc:"Foto 1" }, 
+    {agua: false, src: img2, alt:"foto2", desc:"Foto 2" },
+    {agua: true, src: img3, alt:"foto3", desc:"Foto 3" },
+    {agua: false, src: img4, alt:"foto4", desc:"Foto 4" },
+    {agua: true, src: img5, alt:"foto5", desc:"Foto 5" },
+    {agua: false, src: img6, alt:"foto6", desc:"Foto 6" }
+  ];
+
   return (
-    <>
-      <div class="contenedor">
-          <Imagen
-            agua={true}
-            src={img1}
-            alt="foto1"
-            desc="Foto 1"
-          ></Imagen>
-          <Imagen
-            agua={false}
-            src={img2}
-            alt="foto2"
-            desc="Foto 2"
-          ></Imagen>
-          <Imagen
-            agua={true}
-            src={img3}
-            alt="foto3"
-            desc="Foto 3"
-          ></Imagen>
-          <Imagen
-            agua={false}
-            src={img4}
-            alt="foto4"
-            desc="Foto 4"
-          ></Imagen>
-          <Imagen
-            agua={true}
-            src={img5}
-            alt="foto5"
-            desc="Foto 5"
-          ></Imagen>
-          <Imagen
-            agua={false}
-            src={img6}
-            alt="foto6"
-            desc="Foto 6"
-          ></Imagen>
+      <div className="contenedor">
+          {imgs.map((img,i) => 
+            <Imagen
+              key={`fotoimgs${i+1}`}
+              agua={img.agua}
+              src={img.src}
+              alt={img.alt}
+              desc={img.desc}
+            ></Imagen>
+          )}
       </div>
-    </>
   )
 }
 
